04f3dd8dcabf1d12f68a3581a3b20a1174163234
Rhesa Davinanto <rhesa.davinanto@nadihealth.com>
2022-12-21 18:29:46 +0700

    authentication

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCTm8mFMJNP4opLBjTMQHnUyVSIwnW40o",
+  authDomain: "e-commerce-46e38.firebaseapp.com",
+  databaseURL: "https://e-commerce-46e38-default-rtdb.firebaseio.com",
+  projectId: "e-commerce-46e38",
+  storageBucket: "e-commerce-46e38.appspot.com",
+  messagingSenderId: "179847634542",
+  appId: "1:179847634542:web:4b3d565f5afa8493e3507f",
+  measurementId: "G-MQ398P6M4M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,41 +1,87 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 export default function Login() {
-    let navigate = useNavigate()
+  let navigate = useNavigate();
+  const [errorMsg, setErrorMsg] = useState("");
+  const [showAlert, setAlert] = useState(false);
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    await signInWithEmailAndPassword(auth, e.target[0].value, e.target[1].value)
+    .then((res) => {
+      localStorage.setItem("token", res.user.accessToken);
+      navigate('/home')
+      })
+      .catch((error) => {
+        setErrorMsg(error.message);
+        setAlert(true)
+        if (setAlert) {
+          setTimeout(() => {
+            setAlert(false)
+          },5000)
+        }
+      });
+  };
   return (
     <div className="flex justify-center my-20">
-    <div className="flex flex-col gap-5">
-      <div className="form-control w-full">
-        <label className="label">
-          <span className="label-text w-56">Your Email</span>
-        </label>
-        <label className="input-group">
-          <span className="w-28">Email</span>
-          <input
-            type="text"
-            placeholder="info@site.com"
-            className="input input-bordered w-64"
-          />
-        </label>
-      </div>
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">Your Password</span>
-        </label>
-        <label className="input-group">
-          <span className="w-28">Password</span>
-          <input
-            type="password"
-            placeholder="input your password"
-            className="input input-bordered w-64"
-          />
-        </label>
-      </div>
-      <div className="flex justify-center">
-        <button onClick={()=> navigate('/home')} className="btn">Login</button>
-      </div>
+      <form onSubmit={handleLogin} className="flex flex-col gap-5">
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="label-text w-56">Your Email</span>
+          </label>
+          <label className="input-group">
+            <span className="w-28">Email</span>
+            <input
+              type="text"
+              placeholder="info@site.com"
+              className="input input-bordered w-64"
+            />
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Your Password</span>
+          </label>
+          <label className="input-group">
+            <span className="w-28">Password</span>
+            <input
+              type="password"
+              placeholder="input your password"
+              className="input input-bordered w-64"
+            />
+          </label>
+        </div>
+        <div className="flex justify-center">
+          <button type="submit" className="btn">
+            Login
+          </button>
+        </div>
+      </form>
+      {showAlert ? (
+        <div className="alert alert-error shadow-lg fixed bottom-10 right-10 w-fit cursor-pointer" onClick={()=>setAlert(!showAlert)}>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current flex-shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>{errorMsg}</span>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
-  </div>
-  )
+  );
 }
